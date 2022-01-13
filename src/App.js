0ca6164785078bd0d2b926f4d9f9e4a1e52eb2f4
PPIICCAA <abdulhaidrees@gmail.com>
2022-01-13 16:00:54 +0100

    first page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="users/*" element={<Second />} />
+        <Route path="/series" element={<Second />} />
       </Routes>
     </BrowserRouter>
   );
@@ -30,9 +30,58 @@ function Main() {
       <div>
       <Head />
       <Body />
-      <div className="moreInformation"></div>
-      <div className="playlist"></div>
+      <div class="moreInformation"></div>
+      <div class="playlist"></div>
       </div>
+  )
+}
+
+function Head() {
+  return (
+    <div class="head">
+        <img href="/" class="netflix" src="https://logos-download.com/wp-content/uploads/2016/03/Netflix_Logo_2014.png" />
+        <ol class="list">
+            <li><Link class="link" to="/">Inicio</Link></li>
+            <li><Link class="link" to="/series">Series TV</Link></li>
+            <li><Link class="link" to="/">Películas</Link></li>
+            <li><Link class="link" to="/">Novedades más vistas</Link></li>
+            <li><Link class="link" to="/">Mi lista</Link></li>
+        </ol>
+        <div class="rigth-header">
+            <div><img class="icono search" src={logoSearch} /></div>
+            <div><p>INFANTIL</p></div>
+            <div><img class="icono present" src={presentBox} /></div>
+            <div><img class="icono campana" src={campana} /></div>
+            <div><img class="icono perfil" src={perfile} /><img class="icono arrow" src={arrow} /></div>
+        </div>
+    </div>
+  )
+}
+function Body() {
+  return(
+    <div class="body">
+        <div class="dar">
+            <video id="my-video" class="firstvideo vjs-default-skin" autoplay muted data-setup='{ "techOrder": ["html5", "youtube"] }'></video>
+        </div>
+            <div class="filterdark">
+            <div class="description">
+                <img class="firsttitle" src={serieRandom.titulo} alt=""/>
+                <div class="descriv">
+                </div>
+                <p class="synopsis"></p>
+                <div class="buttons">
+                    <button class="reproducir"><img class="icono" src={play}/> Reproducir</button>
+                    <button class="informacion" onClick="information('videoInformation')"><img class="icono" src={info}/> Más Información</button>
+                </div>
+            </div>
+        </div>
+        <div class="age">
+            <button /* onClick="controlVideo(this, 'my-video')" */ class="play"><img src={mute}></img></button>
+            <div class="agenumber">
+                <h1> w</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 function Second() {
@@ -40,54 +89,6 @@ function Second() {
       <div>
       hola2
       </div>
-  )
-}
-function Head() {
-  return (
-    <div classname="head">
-        <img href="/" classname="netflix" src="https://logos-download.com/wp-content/uploads/2016/03/Netflix_Logo_2014.png" />
-        <ol classname="list">
-            <li><a href="/browser">Inicio</a></li>
-            <li><a href="/browser">Series TV</a></li>
-            <li><a href="/browser">Películas</a></li>
-            <li><a href="/browser">Novedades más vistas</a></li>
-            <li><a href="/browser">Mi lista</a></li>
-        </ol>
-        <div classname="rigth-header">
-            <div><img classname="icono search" src={logoSearch} /></div>
-            <div><p>INFANTIL</p></div>
-            <div><img classname="icono present" src={presentBox} /></div>
-            <div><img classname="icono campana" src={campana} /></div>
-            <div><img classname="icono perfil" src={perfile} /><img classname="icono arrow" src={arrow} /></div>
-        </div>
-    </div>
-  )
-}
-function Body() {
-  return(
-    <div classname="body">
-        <div classname="dar">
-            <video id="my-video" classname="firstvideo vjs-default-skin" autoplay muted data-setup='{ "techOrder": ["html5", "youtube"] }'></video>
-        </div>
-            <div classname="filterdark">
-            <div classname="description">
-                <img classname="firsttitle" src={serieRandom.titulo} alt=""/>
-                <div classname="descriv">
-                </div>
-                <p classname="synopsis"></p>
-                <div classname="buttons">
-                    <button classname="reproducir"><img classname="icono" src={play}/> Reproducir</button>
-                    <button classname="informacion" onClick="information('videoInformation')"><img classname="icono" src={info}/> Más Información</button>
-                </div>
-            </div>
-        </div>
-        <div classname="age">
-            <button /* onClick="controlVideo(this, 'my-video')" */ className="play"><img src={mute}></img></button>
-            <div classname="agenumber">
-                <h1> w</h1>
-            </div>
-        </div>
-    </div>
   )
 }
 export default App;
